c724d36c274b796e46c0f4bfd7b53ec2b7598465
User <artyomaghbalyan@gmail.com>
2021-07-16 01:01:45 +0400

    FEAT: Added Pcr-analysis component

---
+++ component/pcrAnalysisData.tsx
@@ -0,0 +1,50 @@
+const PcrAnalysisData = ()=>{
+    return(
+        <div className="analysis-wrapper">
+            <h3 className="analysis-wrapper__title" >Test Analysis Data</h3>
+            <div className="analysis-wrapper__left-part">
+                <div className="analysis-wrapper__left-part-fam">
+                    <h3 className="analysis-wrapper__left-part-title">FAM</h3>
+                    <div className="analysis-wrapper__results">
+                        <span className="analysis-wrapper__first-result">E Gene</span>
+                        <span className="analysis-wrapper__second-result">+</span>
+                        <span className="analysis-wrapper__third-result">C(t)</span>
+                        <span className="analysis-wrapper__forth-result">29.3</span>
+                    </div>
+
+                </div>
+                <div className="analysis-wrapper__left-part-quasar">
+                    <h3 className="analysis-wrapper__left-part-title">quasar 670</h3>
+                    <div className="analysis-wrapper__results">
+                        <span className="analysis-wrapper__first-result">E Gene</span>
+                        <span className="analysis-wrapper__second-result">+</span>
+                        <span className="analysis-wrapper__third-result">C(t)</span>
+                        <span className="analysis-wrapper__forth-result">29.3</span>
+                    </div>
+                </div>
+            </div>
+            <div className="analysis-wrapper__right-part">
+                <div className="analysis-wrapper__left-part-call-red">
+                    <h3 className="analysis-wrapper__left-part-title">Cal red 61</h3>
+                    <div className="analysis-wrapper__results">
+                        <span className="analysis-wrapper__first-result">E Gene</span>
+                        <span className="analysis-wrapper__second-result">+</span>
+                        <span className="analysis-wrapper__third-result">C(t)</span>
+                        <span className="analysis-wrapper__forth-result">29.3</span>
+                    </div>
+                </div>
+                <div className="analysis-wrapper__left-part-hex">
+                    <h3 className="analysis-wrapper__left-part-title"> HEX</h3>
+                    <div className="analysis-wrapper__results">
+                        <span className="analysis-wrapper__first-result">E Gene</span>
+                        <span className="analysis-wrapper__second-result">+</span>
+                        <span className="analysis-wrapper__third-result">C(t)</span>
+                        <span className="analysis-wrapper__forth-result">29.3</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default PcrAnalysisData
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import Header from "../component/base/header/header";
 import TestResult from "../component/testResult";
 import LabInformation from "../component/labInformation";
 import Footer from "../component/base/footer/footer";
+import PcrAnalysisData from "../component/pcrAnalysisData";
 
 export default function Home() {
   return (
     <div className="carcass">
         <Header/>
         <TestResult/>
+        <PcrAnalysisData/>
         <LabInformation/>
         <Footer/>
     </div>
