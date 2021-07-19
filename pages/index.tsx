41984118433e9499881f8a11a575e36cc5d52d5d
User <artyomaghbalyan@gmail.com>
2021-07-20 00:26:21 +0400

    FEAT: Added antybody component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import TestResult from "../component/testResult";
 import LabInformation from "../component/labInformation";
 import Footer from "../component/base/footer/footer";
 import PcrAnalysisData from "../component/pcrAnalysisData";
+import AntiBodyAnalysisData from "../component/antyBodyAnalysisData";
 
 export default function Home() {
   return (
     <div className="carcass">
         <Header/>
         <TestResult/>
-        <PcrAnalysisData/>
+        <AntiBodyAnalysisData/>
         <LabInformation/>
         <Footer/>
     </div>
