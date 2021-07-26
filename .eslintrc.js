2704163b88e45f463263eaa01d5e2524592314a6
Artyom-Aghbalyan <artyomaghbalyan@gmail.com>
2021-07-27 02:39:53 +0400

    FIX: eslint setting fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,11 @@ module.exports = {
       version: "detect",
     },
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+
+  "extends": "next",
+  "rules": {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
   plugins: ["react", "prettier"],
 }
