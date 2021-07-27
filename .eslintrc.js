306f949ea6ca4cb8cb2b1d7e8c6a8e537a65af1d
User <artyomaghbalyan@gmail.com>
2021-07-27 20:27:33 +0400

    FIX: Fixed build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,10 @@ module.exports = {
       version: "detect",
     },
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: "next",
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
   plugins: ["react", "prettier"],
 }
