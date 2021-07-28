031f9807523cf4418c04932fa115eb1241fd5167
Artyom-Aghbalyan <artyomaghbalyan@gmail.com>
2021-07-29 03:19:46 +0400

    FIX: axios instance fixed

--- manager/AxiosInstance.ts
+++ manager/AxiosInstance.ts
@@ -1,14 +1,28 @@
 import axios from "axios"
+import {guid} from "../utils/guid"
+
+const generateRequestId = () => {
+  return guid();
+
+}
+const getOrGenerateDeviceId = () => {
+  const deviceId = localStorage.getItem("deviceId");
+  if(deviceId){
+    return deviceId
+  } else {
+    const newDeviceId = guid();
+    localStorage.setItem("deviceId", newDeviceId)
+  }
+}
 export const Axios = (token: string) => {
-  console.log(process.env.NEXT_PUBLIC_APP_BASE_URL)
   const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_APP_BASE_URL,
     timeout: 0,
   })
 
   axiosInstance.interceptors.request.use(function (config) {
-    config.headers["opn-device-id"] = process.env.NEXT_PUBLIC_APP_DEVICE_ID
-    config.headers["opn-request-id"] = process.env.NEXT_PUBLIC_APP_REQUEST_ID
+    config.headers["opn-device-id"] = getOrGenerateDeviceId();
+    config.headers["opn-request-id"] = generateRequestId();
     config.headers["opn-lang"] = "en"
     config.headers["captcha-token"] = token
     config.headers["opn-source"] = process.env.NEXT_PUBLIC_APP_SOURCE
