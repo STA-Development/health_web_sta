3fdaa185b2e40fc7d1e51e7a7093f32b3f1d9297
Artyom Aghbalyan <artyomaghbalyan@gmail.com>
2021-08-05 18:36:07 +0400

    FIX: replace test type to dynamic

--- component/testResult.tsx
+++ component/testResult.tsx
@@ -5,7 +5,7 @@ const TestResult = () => {
     const {testResultState} = UseTestResultDataStateValue()
     return (
     <div className={`test-result-wrapper wrapper__${testResultState?.testResult.testType === TestTypes.PCR && testResultState?.testResult.templateId === TestTypes.BioradAntiBody?'biorad-color': testResultState.testResult.result.toLowerCase()}` }>
-        <p className="test-result-text">tested <span className={`span__${testResultState?.testResult.testType === TestTypes.PCR && testResultState?.testResult.templateId === TestTypes.BioradAntiBody?'': testResultState.testResult.result.toLowerCase()}`}>{testResultState?.testResult.result}</span> for sars-cov-2 (rapid Antigen)</p>
+        <p className="test-result-text">tested <span className={`span__${testResultState?.testResult.testType === TestTypes.PCR && testResultState?.testResult.templateId === TestTypes.BioradAntiBody?'': testResultState.testResult.result.toLowerCase()}`}>{testResultState?.testResult.result}</span> for sars-cov-2 (<span>{testResultState.testResult.testType}</span>)</p>
     </div>
   )
 }
