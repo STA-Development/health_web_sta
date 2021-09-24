a97b475246ad6ef97397399250c6fc4d8ac39420
Artyom Aghbalyan <artyomaghbalyan@gmail.com>
2021-09-24 11:02:31 -0400

    FIX: auth middleware

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,33 @@
 import "../styles/scss/main.scss"
 import type {AppProps} from "next/app"
 import {TestResultContextProvider} from "../context/testResultContext"
+import jwt_decode from "jwt-decode";
 import {AuthContextProvider} from "../context/AuthContext";
 import FooterMenu from "../component/base/footer/footerMenu";
 import HeaderMenu from "../component/base/header/headerMenu";
 import Router, {useRouter} from "next/router";
 import {useEffect} from "react";
 
+interface decodedToken {
+    exp: number
+    token: string
+}
+
 function MyApp({Component, pageProps}: AppProps) {
 
     const isAuth = useRouter().route.indexOf("auth") <= -1;
+    const isPublic = useRouter().route === '/';
 
     useEffect(() => {
-        if (localStorage.getItem('accessToken')) {
-            Router.push('/webPortalResult');
+        const token = localStorage.getItem('accessToken')
+        let decodedToken:decodedToken
+        let isExpired
+        if(token) {
+            decodedToken = jwt_decode(token)
+            isExpired = decodedToken.exp < new Date().getTime()
+        }
+        if ((!localStorage.getItem('accessToken') && !isPublic) || isExpired) {
+            Router.push('/auth/login');
         }
     }, []);
 
