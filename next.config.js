b4c7313fcd512c97082413b40aa5055d2ca952a9
Antonyan7 <antonyansergey2002@gmail.com>
2021-09-28 06:19:49 +0400

    FIX: Image Detection from public folder

--- next.config.js
+++ next.config.js
@@ -1,10 +1,35 @@
+/* eslint-disable no-console */
+const domains = []
+try {
+  const url = new URL(process.env.APP_DOMAIN)
+  domains.push(url.hostname)
+} catch (_) {
+  console.error(
+      `APP_DOMAIN is not defined or is invalid url: ${process.env.APP_DOMAIN}`,
+  )
+}
+
+// Use default image load for local
+let images = {}
+// Use public folder with static loaded when building for GCP hosting
+if (process.env.NODE_ENV !== "development") {
+  images = {
+    domains,
+    loader: "imgix",
+    path: `${process.env.APP_DOMAIN}/public`,
+  }
+}
+
 module.exports = {
+  basePath: "",
+  poweredByHeader: false,
+  images,
   reactStrictMode: true,
   env: {
     RECAPTCHA_V3_KEY: process.env.RECAPTCHA_V3_KEY,
     APP_VERSION: process.env.APP_VERSION,
     APP_BASE_URL: process.env.APP_BASE_URL,
-
+    APP_DOMAIN: process.env.APP_DOMAIN,
     APP_SOURCE: process.env.APP_SOURCE,
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     APP_FIREBASE_STORAGEBUCKET: process.env.APP_FIREBASE_STORAGEBUCKET,
