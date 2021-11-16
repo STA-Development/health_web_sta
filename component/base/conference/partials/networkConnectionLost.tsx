f48cb95af2c34287128328fd69afb0b1f28b3c27
Hayk Shahinyan <hayk@stayopn.com>
2021-11-16 18:28:37 +0400

    FEAT: Implement conference room error state regarding to network connection

---
+++ component/base/conference/partials/networkConnectionLost.tsx
@@ -0,0 +1,23 @@
+import NetworkIcon from "../icon/networkIcon";
+
+export default function NetworkConnectionLost(props: {retry?: any}) {
+
+    return (
+        <div className='video-wrapper'>
+            <div className='video-wrapper__content network-content'>
+                <NetworkIcon />
+                <h4>Cannot Connect to a Local Network</h4>
+                <p>
+                    No Internet connection. Make sure Wi-Fi or
+                    <br/>
+                    Cellular data is turned on, then try again.
+                </p>
+                <br/>
+                <br/>
+                <button onClick={props.retry} className='button video-wrapper__button'>
+                    Retry
+                </button>
+            </div>
+        </div>
+    )
+}
--- component/base/conference/video.tsx
+++ component/base/conference/video.tsx
@@ -1,21 +1,40 @@
 import RippleLoader from "./icon/rippleLoader";
+import { useNetworkState } from "react-use";
+import {useEffect, useState} from "react";
+import NetworkConnectionLost from "./partials/networkConnectionLost";
 
 export default function VideoWrapper() {
+    const condition = useNetworkState()
+    const [isOnline, setIsOnline] = useState(true)
+
+    const retryConnecting = () => {
+        if (condition.online) setIsOnline(true)
+    }
+
+    useEffect(() => {
+        if (!condition.online) {
+            setIsOnline(false)
+        }
+    }, [condition.online])
 
     return (
-        <div className='video-wrapper'>
-            <div className='video-wrapper__content'>
-                <RippleLoader/>
-                <h4>You are in the waiting room</h4>
-                <p>
-                    Your call will being as soon as the
-                    <br/>
-                    Health Professional is ready.
-                </p>
-                <br/>
-                <br/>
-                <p>Thank you for your patience!</p>
-            </div>
-        </div>
+        <>
+            {isOnline ? (
+                <div className='video-wrapper'>
+                    <div className='video-wrapper__content'>
+                        <RippleLoader/>
+                        <h4>You are in the waiting room</h4>
+                        <p>
+                            Your call will being as soon as the
+                            <br/>
+                            Health Professional is ready.
+                        </p>
+                        <br/>
+                        <br/>
+                        <p>Thank you for your patience!</p>
+                    </div>
+                </div>
+            ) : <NetworkConnectionLost retry={retryConnecting}/>}
+        </>
     )
 }
