2bf968a053b9827b37e62ef893c14e0fc67635ec
Hayk Shahinyan <hayk@stayopn.com>
2021-11-25 01:30:16 +0400

    FEAT: Implement chat's e2e functionality

--- component/base/conference/chat.tsx
+++ component/base/conference/chat.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react"
+import React, {useEffect, useState} from "react"
 // @ts-ignore
 import * as QB from "quickblox/quickblox"
 import Image from "next/image"
@@ -6,7 +6,13 @@ import {UseConfDataStateValue} from "../../../context/ConferenceContext"
 import Message from "./partials/message"
 import { IMessage, IQBMessage } from "../../../types/context/CnferenceContext"
 
-export default function ChatWrapper() {
+interface IChatWrapper {
+    getMessageValue: (value: string) => void,
+    sendMessage: () => void,
+    messageToSend: string
+}
+
+export default function ChatWrapper({ getMessageValue, sendMessage, messageToSend }: IChatWrapper) {
     const { confDataState, setConfDataState } = UseConfDataStateValue()
     const [messages, setMessages] = useState<IMessage[]>([])
 
@@ -25,52 +31,63 @@ export default function ChatWrapper() {
                 message: messageData.message
             }
         })
-        setMessages([...shortenedMessages])
+        setMessages(shortenedMessages)
     }, [confDataState.messages])
 
     return (
-        <div className='chat-wrapper'>
-            <div className='chat-wrapper__kit-info'>
-                <h4>FH @ Home Travel (Kit Name)</h4>
-                <p>Patient</p>
-                <h5>John Doe</h5>
-            </div>
-            <div className='messenger'>
-                <div className='messenger__header'>
-                    chat
-                </div>
-                <div className='messenger__header messenger__header_mobile'>
-                    <div className='conference-header__logo'>
-                        <Image src='/group.svg' alt='FH HEALTH' width={136} height={16}/>
-                    </div>
-                    <button
-                        onClick={closeMobileChat}
-                        className='button'
-                    >
-                        <Image src="/cross.svg" width={24} height={24} alt="close"/>
-                    </button>
-                </div>
-                <div className='messenger__body'>
-                    {messages?.map((message: IMessage) => (
-                      <Message
-                        key={Math.random()}
-                        messageInfo={message}
+      <div className='chat-wrapper'>
+          <div className='chat-wrapper__kit-info'>
+              <h4>FH @ Home Travel (Kit Name)</h4>
+              <p>Patient</p>
+              <h5>John Doe</h5>
+          </div>
+          <div className='messenger'>
+              <div className='messenger__header'>
+                  chat
+              </div>
+              <div className='messenger__header messenger__header_mobile'>
+                  <div className='conference-header__logo'>
+                      <Image src='/group.svg' alt='FH HEALTH' width={136} height={16}/>
+                  </div>
+                  <button
+                    onClick={closeMobileChat}
+                    className='button'
+                  >
+                      <Image src="/cross.svg" width={24} height={24} alt="close"/>
+                  </button>
+              </div>
+              <div className='messenger__body'>
+                  {messages?.map((message: IMessage) => (
+                    <Message
+                      key={Math.random()}
+                      messageInfo={message}
+                    />
+                  ))}
+              </div>
+              <div className='messenger__footer'>
+                  <form onSubmit={sendMessage}>
+                      <div className='button messenger__footer-button'>
+                          <label htmlFor='upload'>
+                              <Image src='/attach.svg' alt='upload' width={31} height={16}/>
+                          </label>
+                          <input type='file' id='upload'/>
+                      </div>
+                      <input
+                        onChange={(e) => getMessageValue(e.target.value)}
+                        value={messageToSend}
+                        className='input messenger__footer-input'
+                        placeholder='Send Message'
+                        type="text"
                       />
-                    ))}
-                </div>
-                <div className='messenger__footer'>
-                    <div className='button messenger__footer-button'>
-                        <label htmlFor='upload'>
-                            <Image src='/attach.svg' alt='upload' width={31} height={16}/>
-                        </label>
-                        <input type='file' id='upload'/>
-                    </div>
-                    <input className='input messenger__footer-input' placeholder='Send Message' type="text"/>
-                    <div className='button messenger__footer-button'>
-                        <Image src='/send.svg' alt='send' width={24} height={20}/>
-                    </div>
-                </div>
-            </div>
-        </div>
+                      <div
+                        onClick={sendMessage}
+                        className='button messenger__footer-button'
+                      >
+                          <Image src='/send.svg' alt='send' width={24} height={20}/>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
     )
 }
--- pages/conference/room.tsx
+++ pages/conference/room.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 import VideoWrapper from "../../component/base/conference/video";
 import ChatWrapper from "../../component/base/conference/chat";
 // @ts-ignore
@@ -6,13 +6,33 @@ import * as QB from 'quickblox/quickblox.js'
 import { QBConfig } from "../../utils/quickblox/config"
 import {UseConfDataStateValue} from "../../context/ConferenceContext"
 import {IQBMessage} from "../../types/context/CnferenceContext"
+import {load, ReCaptchaInstance} from "recaptcha-v3"
+const kitCode = 'OZC5JK'
 
 export default function ConferenceRoomView() {
   const { confDataState, setConfDataState } = UseConfDataStateValue()
-  const dialogId = '616708512878560019334225'
-  const sessionToken = '91bed4578a7622cd4b9432c3a895a428b200002b'
+  const [dialogId, setDialogId] = useState<string>('')
+  const [userToken, setUserToken] = useState<string>('')
+  const [messageToSend, setMessageToSend] = useState<string>('')
+
+  const getRecaptcha = async () => {
+    const captchaToken = process.env.RECAPTCHA_V3_KEY
+    if (captchaToken) {
+      return await load(captchaToken as string).then((recaptcha: ReCaptchaInstance) => {
+        return recaptcha.execute("submit")
+      })
+    } else {
+      console.error("Captcha token is undefined")
+    }
+  }
+
+  const getMessageValue = (value: string) => {
+    setMessageToSend(value)
+  }
 
   const getSession = () => {
+    console.log(userToken)
+    QB.init(userToken, parseInt(`${process.env.QB_APP_ID}`), null, process.env.QB_ACCOUNT_KEY, QBConfig)
     QB.getSession(function(error: object, { session }: { session: { user_id: number } }) {
       if (error) {
         console.log(error)
@@ -25,13 +45,72 @@ export default function ConferenceRoomView() {
     })
   }
 
+  const connectToChat = () => {
+    const userId = QB.chat.helpers.getUserJid(confDataState.myPersonalId, parseInt(`${process.env.APP_ID}`))
+    const dialogJid = QB.chat.helpers.getRoomJidFromDialogId(dialogId)
+    const userCredentials = {
+      jid: userId,
+      password: userToken
+    }
+
+    QB.chat.connect(userCredentials, function(error: object, contactList: object) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log(contactList)
+      }
+      try {
+        QB.chat.muc.join(dialogJid, function(error2: string, result2: string) {
+          console.log("JOINED ", result2, error2)
+        });
+      } catch (e) {
+        if (e.name === 'ChatNotConnectedError') {
+          console.log("CHAT NOT CONNECTED")
+        }
+      }
+
+      function onMessage(userId: number, message: { body: string }) {
+        getMessagesList()
+        console.log(message, "UPCOMING MESSAGE")
+      }
+
+      QB.chat.onMessageListener = onMessage;
+    });
+
+    QB.chat.onDisconnectedListener = () => {
+      console.log("CHAT DISCONECTED")
+    };
+  }
+
+  const sendMessage = () => {
+    setMessageToSend('')
+    const message = {
+      type: "groupchat",
+      body: messageToSend,
+      extension: {
+        save_to_history: 1,
+        dialog_id: dialogId
+      },
+      markable: 1
+    }
+
+    const dialogJid = QB.chat.helpers.getRoomJidFromDialogId(dialogId)
+    try {
+      QB.chat.send(dialogJid, message)
+      getMessagesList()
+    } catch (e) {
+      if (e.name === 'ChatNotConnectedError') {
+        console.log(e, 'ON_SEND_ERROR')
+      }
+    }
+  }
+
   const getMessagesList = () => {
     const chatParams = {
       chat_dialog_id: dialogId,
       limit: 0,
       mark_as_read: 0,
-      skip: 0,
-      sort_asc: 'date_sent'
+      skip: 0
     }
 
     QB.chat.message.list(chatParams, function(error: object, messages: { items: IQBMessage[] }) {
@@ -46,21 +125,49 @@ export default function ConferenceRoomView() {
     });
   }
 
+  /*const getData = async () => {
+    const captchaToken = await getRecaptcha()
+    try {
+      const result = await conferenceManager.getWaitingToken(captchaToken, kitCode)
+      const waitingToken = result.data.data.waitingToken
+      console.log(waitingToken, 'W-TOKEN')
+      if (waitingToken) {
+        const confCredentials = await conferenceManager.getConferenceCredentials(waitingToken)
+        setDialogId(confCredentials.data.data.dialogId)
+        setUserToken(confCredentials.data.data.userToken)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      await getData()
+    })()
+  }, [])*/
+
   useEffect(() => {
     if (QB.chat) {
+      connectToChat()
       getMessagesList()
     }
   }, [confDataState.myPersonalId])
 
   useEffect(() => {
-    QB.init(sessionToken, parseInt(`${process.env.QB_APP_ID}`), null, process.env.QB_ACCOUNT_KEY, QBConfig)
-    getSession()
-  }, [])
+    if (userToken && dialogId) {
+      getSession()
+    }
+  }, [userToken])
 
   return (
-      <div className='conference-wrapper'>
-          <VideoWrapper/>
-          <ChatWrapper />
-      </div>
+    <div className='conference-wrapper'>
+      <VideoWrapper/>
+      <ChatWrapper
+        getMessageValue={getMessageValue}
+        sendMessage={sendMessage}
+        messageToSend={messageToSend}
+      />
+    </div>
   )
 }
