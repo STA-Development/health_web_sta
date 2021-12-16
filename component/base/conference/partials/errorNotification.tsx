f7b06e8dc8dd7e1f8eb61b08a2db676ba1c92f29
Hayk <h-shahinyan@mail.ru>
2021-12-16 16:26:52 +0400

    FH-1206/Implement chat's error notification UI as in new design (#112)
    * FEaT: Implement code polishing
    
    * FEAT: Implement chat scroll on message send
    
    * FIX: Remove logs from the linted files
    
    * FIX: Adjust code for avoiding extra linting
    
    * FEAT: Implement OK button functionality on kit number popup window
    
    * FEAT: Disable kit number locator UI
    
    * FIX: Adjust changes mentioned in PR
    
    * FIX: Implement changes required in pull request
    
    Co-authored-by: Hayk Shahinyan <hayk@stayopn.com>

--- component/base/conference/partials/errorNotification.tsx
+++ component/base/conference/partials/errorNotification.tsx
@@ -3,14 +3,15 @@ import Image from "next/image"
 export default function ErrorNotification({ isError, setErrorState }: { isError?: boolean, setErrorState: (value: boolean) => void }) {
 
   return (
-    <div className="notification-wrapper">
+    <div className="chat-error-notification">
       <div className={isError ? "error-notification error-notification_active" : "error-notification"}>
         <Image src="/error.svg" width={24} height={24} />
         <p>Something went wrong</p>
         <span
+          className="error-notification__close"
           onClick={() => setErrorState(false)}
         >
-        <Image src="/cross.svg" width={18} height={18} />
+        <Image src="/cross.svg" width={24} height={24} />
       </span>
       </div>
     </div>
