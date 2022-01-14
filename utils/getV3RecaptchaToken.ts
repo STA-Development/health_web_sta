e5b81034b3aabaddf1f57cd82624ba7532996281
Sergei Antonyan <39464948+Antonyan7@users.noreply.github.com>
2022-01-15 01:38:48 +0400

    FIX: Add Singleton instance support for v3 recaptcha loading (#162)

--- utils/getV3RecaptchaToken.ts
+++ utils/getV3RecaptchaToken.ts
@@ -1,14 +1,29 @@
-import {load, ReCaptchaInstance} from 'recaptcha-v3'
+import {getInstance, load} from 'recaptcha-v3'
 import * as Sentry from '@sentry/nextjs'
 
-const googleV3RecaptchaToken = async () => {
-  if (process.env.RECAPTCHA_V3_KEY) {
-    return load(process.env.RECAPTCHA_V3_KEY).then((recaptcha: ReCaptchaInstance) =>
-      recaptcha.execute('submit'),
-    )
+const RECAPTCHA_KEY = process.env.RECAPTCHA_V3_KEY
+const initiateV3Recaptcha = async () => {
+  if (RECAPTCHA_KEY) {
+    return load(process.env.RECAPTCHA_V3_KEY)
   }
   Sentry.captureException('Missing RECAPTCHA V3 KEY')
   return ''
+}
+
+const googleV3RecaptchaToken = async () => {
+  if (RECAPTCHA_KEY) {
+    const recaptchaInstance = getInstance()
+
+    if (!recaptchaInstance) {
+      const newRecaptchaInstance = await initiateV3Recaptcha()
+      if (newRecaptchaInstance) {
+        return newRecaptchaInstance.execute('submit')
+      }
+      Sentry.captureException('RECAPTCHA INSTANCE CREATION FAILED')
+      return ''
+    }
+    return recaptchaInstance.execute('submit')
+  }
 }
 
 export default googleV3RecaptchaToken
