d459f2d1218e8101395aa00cb72768791f55cf18
Sergei Antonyan <39464948+Antonyan7@users.noreply.github.com>
2022-02-24 18:09:56 +0400

    FEAT: Add expired page for public results (#284)

--- component/results/singleResultsPage.tsx
+++ component/results/singleResultsPage.tsx
@@ -15,6 +15,8 @@ import ComponentPreloadView from '@fh-health/component/results/componentPreloadV
 import getV3RecaptchaToken from '@fh-health/utils/getV3RecaptchaToken'
 import VaccineResult from '@fh-health/component/results/vaccineResult'
 import useResultTemplate from '@fh-health/hooks/resultTemplateHook'
+import Image from 'next/image'
+import Card from '@fh-health/component/utils/card'
 
 const SingleTestResultPage = ({isPublicUser}: {isPublicUser: boolean}) => {
   const {testResultState, setTestResultState} = UseTestResultDataStateValue()
@@ -22,6 +24,7 @@ const SingleTestResultPage = ({isPublicUser}: {isPublicUser: boolean}) => {
   const {testResultId} = router.query
   const [resultId, setResultId] = useState<string>('')
   const [loading, setLoading] = useState<boolean>(false)
+  const [isPublicTestResultExpired, setIsPublicTestResultExpired] = useState<boolean>(false)
   const {isAntibodyResult, isBioradAntibodyResult, isPCRResult, isVaccineResult, isCovidFluResult} =
     useResultTemplate()
 
@@ -41,22 +44,35 @@ const SingleTestResultPage = ({isPublicUser}: {isPublicUser: boolean}) => {
   const getData = async () => {
     setLoading(true)
     const token = await getV3RecaptchaToken()
-    try {
-      if (!isPublicUser) {
+    if (!isPublicUser) {
+      try {
         const response = await testResultManager.getSingleTestResult(resultId as string)
         if (response.status === 200) {
           const {data} = response.data
           setTestResultState({type: TestResultContextStaticData.UPDATE_TEST_RESULT, data})
         }
-      } else if (token && testResultId) {
+      } catch (err) {
+        const errorObject = {
+          err,
+          message: 'Private Test Result Load Failed',
+        }
+        Sentry.captureException(errorObject)
+      }
+    } else if (token && testResultId) {
+      try {
         const response = await testResultManager.getTestResult(token, resultId as string)
         if (response.status === 200) {
           const {data} = response.data
           setTestResultState({type: TestResultContextStaticData.UPDATE_TEST_RESULT, data})
         }
+      } catch (err) {
+        setIsPublicTestResultExpired(true)
+        const errorObject = {
+          err,
+          message: 'Public Test Result Load Failed',
+        }
+        Sentry.captureException(errorObject)
       }
-    } catch (err) {
-      Sentry.captureException(err)
     }
     setLoading(false)
   }
@@ -89,7 +105,33 @@ const SingleTestResultPage = ({isPublicUser}: {isPublicUser: boolean}) => {
           <Footer />
         </div>
       ) : (
-        <ComponentPreloadView />
+        <div className="pure-block-wrapper">
+          {isPublicTestResultExpired ? (
+            <div className="card-wrapper">
+              <Card permissions={false}>
+                <div className="card__media card__media_sm">
+                  <Image src="/error-cross.svg" alt="kit number" height={64} width={64} />
+                </div>
+                <div className="card__content">
+                  <h4 className="card__content-title">Access Code Expired</h4>
+                  <p className="card__content-message">
+                    QR code link has expired. Please contact us <br />
+                    through the chat or fhhealth.com for any questions or concerns.
+                  </p>
+                </div>
+              </Card>
+              <p className="card-wrapper__message">
+                Need help? <br />
+                Live Chat available on{' '}
+                <a href="https://www.fhhealth.com/" className="em-link">
+                  fhhealth.com
+                </a>
+              </p>
+            </div>
+          ) : (
+            <ComponentPreloadView />
+          )}
+        </div>
       )}
     </div>
   )
