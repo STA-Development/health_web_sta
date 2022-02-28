2022335751d6112dc00425bdab96594959b08e2f
Sergei Antonyan <39464948+Antonyan7@users.noreply.github.com>
2022-02-28 23:29:31 +0400

    FIX: Update color for vaccine results (#289)

--- component/results/vaccineResult.tsx
+++ component/results/vaccineResult.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const VaccineResult = () => (
-  <div className="testResult-wrapper">
+  <div className="testResult-wrapper vaccine-result">
     <p className="testResult-text">STATUS: COMPLETED VACCINATION APPOINTMENT</p>
   </div>
 )
